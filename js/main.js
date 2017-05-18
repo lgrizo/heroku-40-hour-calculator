69a303f00fd8ddc22415decee10dc94b38713f69
Luis Rizo <rizo.luis@yahoo.com>
2017-05-17 21:48:38 -0400

    set up files

---
+++ js/main.js
@@ -0,0 +1,16 @@
+//
+// change border color to teal when input is clicked
+
+// const userInput = document.getElementById("userInput");
+//   userInput.addEventListener("click", () => {
+//     userInput.style.borderColor = "#15D1BC";
+// });
+
+// next
+
+var hoursWeek = document.getElementById('hoursWeek');
+var fridayMorning = document.getElementById('fridayMorning');
+var remainingDecimals = document.getElementById('remainingDecimals');
+
+var convertToTime = fridayMorning + remainingDecimals;
+console.log(convertToTime);
