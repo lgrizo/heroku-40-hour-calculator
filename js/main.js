55b22475363487de60d9b05a9307e4e66af23cda
Luis Rizo <rizo.luis@yahoo.com>
2017-05-30 21:03:59 -0400

    updated

--- js/main.js
+++ js/main.js
@@ -12,13 +12,36 @@
 
 
 function calculateButton() {
-  var fridayMorning = parseInt(document.getElementById('fridayMorning').value);
-  var remainingDecimals = (document.getElementById('remainingDecimals').value);
-  var roundedDecimal = Math.round(remainingDecimals * 60);
 
+  // current hours on friday morning
+  let fridayMorning = parseInt(document.getElementById('fridayMorning').value);
+
+  //minutes in decimal form
+  let remainingDecimals = (document.getElementById('remainingDecimals').value);
+
+  //convert decimal minutes into time format minutes
+  let roundedDecimal = Math.round(remainingDecimals * 60);
+
+  //result for current hours worked
   let currentWorked = fridayMorning + " hours and " + roundedDecimal + " minutes";
 
+
+  //this is to display the current hours worked results in the correct field on page
   document.getElementById('currentWorked').innerHTML = currentWorked;
+
+
+
+
+  let hoursThisWeek = parseInt(document.getElementById('hoursThisWeek').value);
+
+  let remainingHours = (hoursThisWeek - fridayMorning) - 1;
+
+  let remainingMinutes = (60 - roundedDecimal);
+
+  let hoursLeftToWork = remainingHours + " hours and " + remainingMinutes + " minutes"
+
+  document.getElementById('hoursLeftToWork').innerHTML = hoursLeftToWork;
+
 
 
 }
