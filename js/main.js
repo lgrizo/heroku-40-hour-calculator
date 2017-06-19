20c5e5a4ecbaf1ceeb26d0836a8886d27fcdf9a9
Luis Rizo <rizo.luis@yahoo.com>
2017-06-18 22:33:25 -0400

    big changes

--- js/main.js
+++ js/main.js
@@ -1,39 +1,50 @@
-//
-
 function calculateButton() {
+	// this gets the value for the "hours planned on working this week" field
+	var hoursThisWeek = parseInt(document.getElementById("hoursThisWeek").value);
 
-  // this gets the value for the "hours planned on working this week" field
-  let hoursThisWeek = parseInt(document.getElementById('hoursThisWeek').value);
+	// current hours on friday morning
+	var fridayMorning = parseInt(document.getElementById("fridayMorning").value);
 
-  // current hours on friday morning
-  let fridayMorning = parseInt(document.getElementById('fridayMorning').value);
+	// ramaining minutes in decimal form
+	var remainingDecimals = parseFloat(document.getElementById("remainingDecimals").value);
+	remainingDecimals = Math.round(remainingDecimals * 60);
 
-  // ramaining minutes in decimal form
-  let remainingDecimals = (document.getElementById('remainingDecimals').value);
+	// ramaining minutes in decimal form
+	var clockedInFri = document.getElementById("enterClockIn").value;
 
-  //convert decimal minutes into time format minutes rounded to the nearest whole number
-  let roundedDecimal = Math.round(remainingDecimals * 60);
+	// ramaining minutes in decimal form
+	var lunchBreak = parseInt(document.getElementById("enterLunch").value);
 
-  //result for current hours worked
-  let currentWorked = fridayMorning + " hours " + roundedDecimal + " minutes";
+	hoursThisWeek = hoursThisWeek * 60;
 
-  // var min =
-  //   if (roundedDecimal === 1) {
-  //     "minute";
-  //   } else {
-  //     "minutes";
-  //   }
 
-  let remainingHours = (hoursThisWeek - fridayMorning) - 1;
+	var currentWorked = fridayMorning * 60 + remainingDecimals;
+	//
+	var currentWorkedMinutes = currentWorked % 60;
+	var currentWorkedOutput =
+		fridayMorning + " hours " + currentWorkedMinutes + " minutes";
+	//
+	var remaining = hoursThisWeek - currentWorked;
+	if (remaining <= 0) {
+		return; //stop the function from executing if user has worked more than planned. You should show a message to the user if he has worked more than planned already
+	}
+	var remainingHours = Math.floor(remaining / 60);
+	var remainingMinutes = remaining % 60;
+	var remainingOutput =
+		remainingHours + " hours " + remainingMinutes + " minutes";
 
-  let remainingMinutes = (60 - roundedDecimal);
+	clockedInFri = moment(clockedInFri, "hh:mm A");
 
-  let hoursLeftToWork = remainingHours + " hours " + remainingMinutes + " minutes";
-
-  //this is to display the current hours worked results
-  document.getElementById('currentWorked').innerHTML = currentWorked;
-
-  //this is to display the remaining hours left results
-  document.getElementById('hoursLeftToWork').innerHTML = hoursLeftToWork;
-
+	var clockOutTime = moment(clockedInFri, "hh:mm A").add(
+		remaining + lunchBreak,
+		"minutes"
+	);
+	clockOutTime = clockOutTime.format("hh:mm A");
+	//this is to display the current hours worked results
+	document.getElementById("currentWorked").innerHTML = currentWorkedOutput;
+	//this is to display the remaining hours left results
+	document.getElementById("hoursLeftToWork").innerHTML = remainingOutput;
+	//this is to display the current hours worked results
+	document.getElementById("clockOutFri").innerHTML = clockOutTime;
+	console.log(moment());
 }
